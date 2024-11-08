eee41f7acee92e3f9b4ccf4125b01768ca793f81
nahbee10 <nahee@sanctuary.computer>
2024-11-08 09:47:42 -0500

    connect side nav to request page

--- src/modules/navigation/Navigation.tsx
+++ src/modules/navigation/Navigation.tsx
@@ -1,14 +1,14 @@
-import { FC, useState, useEffect, useRef } from "react";
-import type { Navigation } from "./data-types";
+import { FC, useState, useEffect, useRef } from 'react';
+import type { Navigation } from './data-types';
 
 import cn from 'classnames';
 
-import { MiniUnicon } from "../../svgs/Logos";
-import { Menu, Search } from "../../svgs/Icons";
-import { PrimaryButton, ButtonBase, LinkBase } from "../../base/Button";
-import ThemeSwitch from "./ThemeSwitch";
-import MobileMenuModal from "./MobileMenuModal";
-import { UIProvider } from "../../context/uiProvider";
+import { MiniUnicon } from '../../svgs/Logos';
+import { Menu, Search } from '../../svgs/Icons';
+import { PrimaryButton, ButtonBase, LinkBase } from '../../base/Button';
+import ThemeSwitch from './ThemeSwitch';
+import MobileMenuModal from './MobileMenuModal';
+import { UIProvider } from '../../context/uiProvider';
 
 const Navigation: FC = () => {
   const [scrollIsOnTop, setScrollIsOnTop] = useState(false);
@@ -35,15 +35,15 @@ const Navigation: FC = () => {
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      const searchBarPlaceholder = document.getElementById("search-bar-placeholder-nav-mobile");
+      const searchBarPlaceholder = document.getElementById('search-bar-placeholder-nav-mobile');
       if (searchBarPlaceholder && searchBarRef.current) {
         searchBarRef.current.appendChild(searchBarPlaceholder);
-        searchBarPlaceholder.style.opacity = "1";
+        searchBarPlaceholder.style.opacity = '1';
       }
     }, 100);
 
     return () => clearTimeout(timeout);
-  }, []); 
+  }, []);
 
   return (
     <UIProvider>
@@ -55,9 +55,14 @@ const Navigation: FC = () => {
           }
         )}
       >
-        <div className={cn("absolute w-full h-full top-0 left-0 !bg-light-surface-1 dark:!bg-dark-surface-1 px-4 py-[1.15625rem] flex flex-row justify-between items-center", {
-          "hidden": !mobileSearchBarIsOpen
-        })}>
+        <div
+          className={cn(
+            'absolute w-full h-full top-0 left-0 !bg-light-surface-1 dark:!bg-dark-surface-1 px-4 py-[1.15625rem] flex flex-row justify-between items-center',
+            {
+              hidden: !mobileSearchBarIsOpen,
+            }
+          )}
+        >
           <div className="flex flex-row items-center grow">
             <Search className="h-padding-large w-padding-large" />
             <div ref={searchBarRef} className="grow"></div>
@@ -73,7 +78,7 @@ const Navigation: FC = () => {
         </div>
         <div className="flex w-full flex-row items-center justify-between border-light-surface-3 px-4 py-[1.15625rem] md:px-[0.9375rem] md:py-3 md:h-[4.5rem]">
           <div className="flex flex-row items-center">
-            <LinkBase href="/" className="flex flex-row items-center">
+            <LinkBase href="/hc/en-us" className="flex flex-row items-center">
               <MiniUnicon className="mb-[0.1875rem] h-8 w-8" />
               <p className="body-3 md:button-label-2 ml-2 text-light-accent-1 dark:text-dark-accent-1">
                 Uniswap Support
@@ -103,7 +108,7 @@ const Navigation: FC = () => {
             <PrimaryButton
               className="ml-padding-small-dense !my-auto !py-0 !h-8"
               label="Submit Request"
-              href="https://support.uniswap.org/hc/en-us/requests/new"
+              href="/hc/en-us/requests/new"
               color="accent-2"
             />
           </div>
